d64d5f85cd7c96cb2e44a5256c3aae6b3871be2d
anupamam <anupamamann07@gmail.com>
2014-07-15 20:55:56 -0700

    View Pager implementation

--- src/com/android/apps/eventplanner/ItemDetailActivity.java
+++ src/com/android/apps/eventplanner/ItemDetailActivity.java
@@ -3,35 +3,140 @@ package com.android.apps.eventplanner;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.android.apps.eventplanner.ItemDetailTestActivity.MyPagerAdapter;
 import com.android.apps.eventplanner.fragments.FoodMenuFragment;
+import com.android.apps.eventplanner.models.FoodMenu;
+import com.android.apps.eventplanner.utils.SmartFragmentStatePagerAdapter;
 
 public class ItemDetailActivity extends FragmentActivity {
-	FoodMenuFragment menuList;
+	SmartFragmentStatePagerAdapter adapterViewPager;
+	ViewPager vpPager;
+	
+	ImageButton btAdd;
+	ImageButton btNext;
+	ImageButton btPrevious;
+	
+	
+	public static class MyPagerAdapter extends SmartFragmentStatePagerAdapter  {
+		private static int NUM_ITEMS = 3;
+		static String fragmentName;
+		static String fragmentInstance;
+		
+		public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+        
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+        
+        public static void setNumCount(int total_pages){
+        	NUM_ITEMS = total_pages;
+        }
+        
+        /**
+         * 
+         * @param totalPages - Number of Objects in the list view to be displayed 
+         * @param customFragmentClassName - Custom Fragment   
+         * @param staticInstanceMethod - static method 
+         */
+        public static void setFragment(int totalPages, String customFragmentClassName, String staticMethod){
+        	NUM_ITEMS = totalPages;
+        	fragmentName = customFragmentClassName;
+        	fragmentInstance = staticMethod;
+        }
+        
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+        	// return Class.forname(fragmentName).getMethod(fragmentInstance, null).invoke(page, array).
+              return FoodMenuFragment.newInstance(position,  FoodMenu.getFoodMenu().get(position));
+        }
+         
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+        	return "Page " + position;
+        }
+        
+    }
+
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_item_detail);
+		setContentView(R.layout.activity_item_detail_test);
 		//getActionBar().setDisplayShowHomeEnabled(false);
 		getActionBar().setDisplayShowTitleEnabled(false);
-		
 		View mActionBarView = getLayoutInflater().inflate(R.layout.my_action_bar, null);
 		getActionBar().setCustomView(mActionBarView);
 		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 		
+		
+		btAdd = (ImageButton)findViewById(R.id.btnAdd);
+		//btNext = (ImageButton)findViewById(R.id.btnNext);
+		//btPrevious = (ImageButton)findViewById(R.id.btnPrevious);
+		
+		vpPager = (ViewPager) findViewById(R.id.vpPager);
+		adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+		
 		//you must be coming on this activity from a particular to do list item
 		//based on the item in to do ..pull up the fragment to display
 		//I'm pulling foodMenu fragment as of now
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		ft.replace(R.id.flContainer, new FoodMenuFragment());
-		ft.commit();
+		MyPagerAdapter.setFragment(FoodMenu.getFoodMenu().size(), "FoodMenuFragment", "newInstance");
+		vpPager.setAdapter(adapterViewPager);
+		
+		 vpPager.setOnPageChangeListener(new OnPageChangeListener() {
+	        	
+	        	// This method will be invoked when a new page becomes selected.
+	        	@Override
+	        	public void onPageSelected(int position) {
+	        		Toast.makeText(ItemDetailActivity.this,"Selected page position: " + position, Toast.LENGTH_SHORT).show();
+	        	}
+	        	
+	        	// This method will be invoked when the current page is scrolled
+	        	@Override
+	        	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+	        		// Code goes here, handle the case where 
+	        	}
+	        	
+	        	// Called when the scroll state changes: 
+	        	// SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+	        	@Override
+	        	public void onPageScrollStateChanged(int state) {
+	        		// Code goes here
+	        	}
+	        });
+		
+		
+		 btAdd.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					//get FoodList 
+					FoodMenuFragment selectedFood = (FoodMenuFragment)adapterViewPager.getRegisteredFragment(vpPager.getCurrentItem());
+					//save object into current event
+					Toast.makeText(ItemDetailActivity.this, selectedFood.getFood().getUrls().toString(), Toast.LENGTH_LONG).show();
+				}
+			});
 		
 	}
 	
---
+++ src/com/android/apps/eventplanner/utils/SmartFragmentStatePagerAdapter.java
@@ -0,0 +1,50 @@
+package com.android.apps.eventplanner.utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
+
+public class SmartFragmentStatePagerAdapter extends FragmentPagerAdapter {
+
+	// Sparse array to keep track of registered fragments in memory
+		private SparseArray<Fragment> registeredFragments = new SparseArray<Fragment>();
+
+		public SmartFragmentStatePagerAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
+		}
+
+		// Register the fragment when the item is instantiated
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			Fragment fragment = (Fragment) super.instantiateItem(container, position);
+			registeredFragments.put(position, fragment);
+			return fragment;
+		}
+
+		// Unregister when the item is inactive
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			registeredFragments.remove(position);
+			super.destroyItem(container, position, object);
+		}
+
+		// Returns the fragment for the position (if instantiated)
+		public Fragment getRegisteredFragment(int position) {
+			return registeredFragments.get(position);
+		}
+
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+}
